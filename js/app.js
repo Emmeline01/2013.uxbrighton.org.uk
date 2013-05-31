cb4b8f0208a0fe48bb70ad10771d611cbf8e0c90
Jay Caines-Gooby <jay@gooby.org>
2013-05-31 05:46:07 +0100

    actually do the submit to google

--- js/app.js
+++ js/app.js
@@ -2,19 +2,26 @@ $(function(){
 
   // grab their email address, put it into the google doc email
   // form element and subit the form in the iframe
-  
-  // minimal validation - just look for @  
   $('#email input[type=email]').keyup(function(){    
+    
+    var submitButton = $('#email input[type=submit]').attr('disabled', false);
+    
+    // minimal validation - just look for @  
     if ($(this).val().match(/@/)) {
-      $('#email input[type=submit]').attr('disabled', false);
+      submitButton.attr('disabled', false);
+    } else {
+      submitButton.attr('disabled', true);
     }
   });
-  $('#email input[type=submit]').click(function(){
+  
+  $('#email form').submit(function(e){
+    
+    e.preventDefault();
     // copy email address
-    $('#entry_0', window.top.frames[0].document).val($('#email').val()); 
+    $('#entry_0', window.top.frames[0].document).val($('#email input[type=email]').val()); 
     
     // submit the form
-    //$('#email-form input[type=submit]', window.top.frames[0].document).click();
+    $('#email-form input[type=submit]', window.top.frames[0].document).click();
 
     // we'll just assume that it got posted
     $('#email input').hide();
